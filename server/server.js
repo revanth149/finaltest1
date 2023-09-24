aa9426ed95327523e536e2253095b7ce1915f37e
revanth149 <gallarevanth143@gmail.com>
2023-09-24 13:38:10 -0400

    modified to angular structure

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const fs= require('fs');
-
-app.use('/', express.static('public'));
+app.use(cors());
+// app.use('/', express.static('public'));
 
 // const budget = {
 //     myBudget: [
